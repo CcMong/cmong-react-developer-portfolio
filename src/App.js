5c3a2d825ec5959f1c932ffa8505f6af06b7ebe5
CcMong <fullstackcodex@gmail.com>
2023-03-06 11:00:55 +0000

    Imported the Header function and BrowserRouter into App.js. Then I placed the Header component within a Router element wrapper, but outside the Route elements which I will use for nav links to other pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
 
 
 function App() {
   return (
-    <div>
-            
-    </div>
+    <Router>
+      {/*Place Header here outside the div so that it will show on every page */}
+      <Header />
+      <div>
+        {/*Navigation links will point to other pages. So will use Route elements for each link, and will group them within a Routes element */}
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </div>            
+    </Router>
   );
 }
 

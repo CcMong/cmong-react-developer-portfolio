66d69e0b70df731d8da774cf580bfc4de0115fd3
CcMong <fullstackcodex@gmail.com>
2023-03-06 11:34:53 +0000

    Created a Home function within the Home.js file, put basic information on it, exported it to the App.js file, and placed it within a Route element as the site home page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
+import Home from "./components/webpages/Home";
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <div>
         {/*Navigation links will point to other pages. So will use Route elements for each link, and will group them within a Routes element */}
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>            
     </Router>
---
+++ src/components/webpages/Home.js
@@ -0,0 +1,15 @@
+
+function Home() {
+
+    return (
+
+        <main>
+            <h1>Home Page</h1>
+            <p>
+                This is the home page. It will have an intro, jumbotron with my photo, brand statement, and some graphics to showcase my range of skills and services.
+            </p>
+        </main>
+    )
+}
+
+export default Home;
